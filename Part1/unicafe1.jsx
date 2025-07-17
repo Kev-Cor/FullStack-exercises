9fb233b9bd363288ba95ff7fdfd3e5af22246df1
Kev-Cor <164213839+Kev-Cor@users.noreply.github.com>
2025-07-17 11:37:01 -0600

    Create unicafe1.jsx
    this application just adds three buttons suggesting the reviews received.

---
+++ Part1/unicafe1.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [allReviews, setAll] = useState(0)
+
+  const handleGood = () => {
+    setGood(good+1)
+    setAll(allReviews+1)
+  }
+  const handleNeutral = () => {
+    setNeutral(neutral+1)
+    setAll(allReviews+1)
+  }
+  const handleBad = () => {
+    setBad(bad+1)
+    setAll(allReviews+1)
+  }
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.jsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+      <div>
+          <h3>Feedback</h3>
+          <p>Good review: {good}</p>
+          <p>Neutral Reviews: {neutral}</p> 
+          <p>Bad Reviews: {bad}</p>
+          <p>
+            <button onClick={handleGood}>Good</button>
+            <button onClick={handleNeutral}>Neutral</button>
+            <button onClick={handleBad}>Bad</button>
+          </p>
+      </div>
+    </>
+  )
+}
+
+export default App
